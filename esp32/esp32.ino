848cba52e844e106f3ef99284d1a6b911c752857
JJK-23 <j.j.knief@student.tue.nl>
2026-09-26 17:23:57 +0200

    Changed ESP code to contain count logic and 4 reed switches, works perfectly now :)

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -4,7 +4,11 @@
 // WiFi
 const char* ssid = "";
 const char* password = "";
-const int reedPin = 14;
+const int reedPin1 = 13;
+const int reedPin2 = 14;
+const int reedPin3 = 15;
+const int reedPin4 = 16;
+int previousState = 0;
 
 // OOCSI
 const char* OOCSIName = "ESP32";
@@ -12,39 +16,56 @@ const char* hostserver = "oocsi.id.tue.nl";
 
 OOCSI oocsi = OOCSI();
 
-void processOOCSI() {
-}
-
 void setup() {
-  pinMode(reedPin, INPUT_PULLUP);
-  Serial.begin(9600);
-  delay(2000);
 
-  Serial.println("Connecting to OOCSI...");
+  pinMode(reedPin1, INPUT_PULLUP);
+  pinMode(reedPin2, INPUT_PULLUP);
+  pinMode(reedPin3, INPUT_PULLUP);
+  pinMode(reedPin4, INPUT_PULLUP);
+
+  Serial.begin(115200);
+  delay(2000);
 
   oocsi.connect(
     "ESP32",
     "oocsi.id.tue.nl",
     ssid,
-    password,
-    processOOCSI
+    password
   );
-
-  Serial.println("oocsi.connect() returned");
 }
 
-void loop() {
-  if (digitalRead(reedPin) == LOW) 
+void loop() 
+{
+  int currentState = 0;
+
+  if (digitalRead(reedPin1) == LOW) 
   {
-    Serial.println("Magnet Found");
-    oocsi.newMessage("ESP-test");
-    oocsi.addString("Message", "Magnet found");
-    oocsi.sendMessage();
+    currentState += 1;
   } 
-  else 
+
+  if (digitalRead(reedPin2) == LOW) 
   {
-    Serial.println("Magnet Lost");
+    currentState += 1;
+  } 
+
+  if (digitalRead(reedPin3) == LOW) 
+  {
+    currentState += 1;
+  } 
+
+  if (digitalRead(reedPin4) == LOW) 
+  {
+    currentState += 1;
+  } 
+
+  if (currentState != previousState)
+  {
+    oocsi.newMessage("ESP_Test_Team_1");
+    oocsi.addInt("Message", currentState);
+    oocsi.sendMessage();
+    previousState = currentState;
   }
+
 
   delay(1000);
 }
